f16b93cf005f95dc13330d50e781659523ec7380
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2021-04-25 11:29:05 +0200

    Basic JUnit test exercise

---
+++ src/test/java/StringHelperTest.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringHelperTest {
+
+    StringHelper helper = new StringHelper();
+
+    @Test
+    public void testTruncateAInFirst2Positions_AInFirst2Positions() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_AInFirstPosition() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_ANotInFirstPosition() {
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
+        assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_OneCharacterString() {
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame("A"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_EmptyString() {
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame(""));
+    }
+}
