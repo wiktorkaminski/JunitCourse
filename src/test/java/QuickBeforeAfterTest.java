5a28fdc264c2c31d90e405354a7f6f435cec36b2
wiktorkaminski <wiktor.kaminski@mixbox.pl>
2021-04-25 11:42:40 +0200

    Before, after

---
+++ src/test/java/QuickBeforeAfterTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class QuickBeforeAfterTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void setup() {
+        System.out.println("Before Test");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("test1 executed");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("test2 executed");
+    }
+
+    @After
+    public void teardown() {
+        System.out.println("After Test");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
+
+}
--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -1,10 +1,16 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class StringHelperTest {
 
-    StringHelper helper = new StringHelper();
+    StringHelper helper;
+
+    @Before
+    public void before() {
+        helper = new StringHelper();
+    }
 
     @Test
     public void testTruncateAInFirst2Positions_AInFirst2Positions() {
